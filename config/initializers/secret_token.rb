2b06a54f9132aef09155652f3d6ee7c7bbe6160f
Pat Shaughnessy <pat@patshaughnessy.net>
2012-03-14 14:02:01 -0400

    Empty RefineryCMS app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SaintRock::Application.config.secret_token = 'e5251ce23b41810e3e39f6748de049c046d3793ca087f62328414cbd6ed026c77852094a60389a8fc9bbe3336a16f712da4c899285139475f09bda62f559fc2b'
