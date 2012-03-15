cc0a135765e170c99e7f098782c3dc90673093d1
Pat Shaughnessy <pat@patshaughnessy.net>
2012-03-15 16:45:39 -0400

    overlap header image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  def overlap_class(page)
+    puts "DEBUG #{page.slug}"
+    case page.slug
+    when 'home'
+      'header-overlap-image1'
+    when 'services'
+      'header-overlap-image1'
+    when 'photo-gallery'
+      ''
+    else
+      'header-overlap-image2'
+    end
+  end
 end
